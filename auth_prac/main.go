b52d54c9b4fa22d8b799d3263519b5de0a34ba97
Eric Ekholm <eric.ekholm@gmail.com>
2022-10-21 07:09:26 -0400

    start logging user in functions

--- auth_prac/main.go
+++ auth_prac/main.go
@@ -6,12 +6,15 @@ package main
 //https://gist.github.com/mrcrilly/7703d630f9d589636d20b630245b6415
 
 import (
+	"errors"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
-//a user
+// a user
 type User struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
@@ -19,29 +22,82 @@ type User struct {
 
 var users []*User
 
-//a struct that we get register input from
+// a struct that we get register input from
 type RegisterInput struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 
-//register a user from URL parameters
-//see https://github.com/gin-gonic/gin#querystring-parameters
+// register a user from URL parameters
+// see https://github.com/gin-gonic/gin#querystring-parameters
 func Register(c *gin.Context) {
 	u := c.Query("username")
 	pw := c.Query("password")
-
-	//var user *User
 
 	user := &User{
 		Username: u,
 		Password: pw,
 	}
 
+	err := user.hashPw()
+
+	//this isn't the best way to do this, but w/e for now
+	if err != nil {
+		log.Fatal("Failed to hash password")
+	}
+
 	//this sort of works, but it will keep duplicates
+	//not really the point of this code to get rid of it, though
 	users = append(users, user)
 
 	c.JSON(http.StatusOK, gin.H{"user_list": users})
+}
+
+// creating a function to hash the password
+func (u *User) hashPw() error {
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return err
+	}
+
+	u.Password = string(hashedPass)
+
+	return nil
+}
+
+func (u *User) checkUserExists() (int, error) {
+	for i, v := range users {
+		if u.Username == v.Username {
+			return i, nil
+		}
+	}
+	return 0, errors.New("user doesn't exist")
+}
+
+// allow login
+func Login(c *gin.Context) {
+	//get params from querystring
+	u := c.Query("username")
+	pw := c.Query("password")
+
+	inpUser := &User{
+		Username: u,
+		Password: pw,
+	}
+
+	if len(users) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "no users registered"})
+		return
+	}
+
+	ind, err := inpUser.checkUserExists()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+	}
+
+	//RESUME HERE
 }
 
 func main() {
