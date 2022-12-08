8709e85796ef402b7f1ace7887f0825a7cd47389
Eric Ekholm <eric.ekholm@gmail.com>
2022-12-08 15:12:41 -0500

    more fiddling with gorilla mux and stdlib

--- basic_web_app/main.go
+++ basic_web_app/main.go
@@ -3,7 +3,7 @@ package main
 
 import (
 	"encoding/json"
-	"log"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -13,16 +13,29 @@ const (
 	listenAddr string = ":8080"
 )
 
+type User struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+var users []*User
+
 func main() {
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", indexHandler).Methods("GET")
+	r.HandleFunc("/content/{x}", handleContent).Methods("GET")
+	r.HandleFunc("/user", handleCreateUser).Methods("POST")
+	r.HandleFunc("/user", handleGetUser).Methods("GET")
 
 	//create the server
 	s := &http.Server{
 		Addr:    listenAddr,
 		Handler: r,
 	}
+
+	fmt.Println("Running basic web app")
 
 	//run the server
 	s.ListenAndServe()
@@ -35,19 +48,41 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 	resp["hello"] = "world"
 
-	jResp, err := json.Marshal(resp)
-
-	if err != nil {
-		log.Fatalf("couldn't marshal json data: %s", err)
-	}
-
-	//set status
-	w.WriteHeader(http.StatusOK)
-
 	//set content type as JSON
 	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 
-	//write this out as json
-	w.Write(jResp)
-	//this is currently showing up as plain text, but we can fix this later
+	json.NewEncoder(w).Encode(resp)
+
+}
+
+func handleContent(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	resp := make(map[string]string)
+
+	resp["content"] = vars["x"]
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+func handleCreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	u := User{}
+
+	json.NewDecoder(r.Body).Decode(&u)
+
+	users = append(users, &u)
+}
+
+func handleGetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(users)
 }
